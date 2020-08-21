0911cf6496ef11a28d43cad328f93f6275a4c902
meierjonas96@web.de <meierjonas96@web.de>
2020-08-21 15:46:33 +0200

    Add stub for Faster-RCNN-50-FPN detection for DoorDetect-Dataset with comments to better understand the config.

---
+++ configs/doordetect/faster_rcnn_r50_fpn_1x_doordetect.py
@@ -0,0 +1,43 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/doordetect_detection.py',
+    '../_base_/default_runtime.py'
+]
+model = dict(
+    pretrained=None,  # ImageNet pretrained backbone to be loaded
+        # (e.g. 'torchvision://resnet50' or 'torchvision://resnet101')
+    # backbone=dict(),
+    # neck=dict(),
+    # rpn_head=dict(),
+    # roi_head=dict(),
+        #  TODO: continue
+)
+# optimizer. lr=0.02 is for bs=16! More types at 'mmdet/core/optimizer/default_constructor.py'
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)  # Most methods do not use gradient clipping
+# learning policy
+lr_config = dict(  # Learning rate scheduler config used to register LrUpdater hook
+    policy='step',  # The policy of scheduler, more supported, see: 'mmcv/runner/hooks/lr_updater.py'
+    warmup='linear',  # The warmup policy, also support `exp` and `constant`
+    warmup_iters=500,  # The number of iterations for warmup
+    warmup_ratio=0.001,  # The ratio of the starting learning rate used for warmup
+    step=[8, 11])  # Steps to decay the learning rate
+total_epochs = 12  # Total epochs to train the model TODO: 1 epoch = 1 whole dataset? epochs == times dataset repeated?
+checkpoint_config = dict(  # Config to set the checkpoint hook, see: 'mmcv/runner/hooks/checkpoint.py'
+    interval=1)  # The save interval is 1
+log_config = dict(  # config to register logger hook
+    interval=50,  # Interval to print the log
+    hooks=[
+        # dict(type='TensorboardLoggerHook')  # The Tensorboard logger is also supported
+        dict(type='TextLoggerHook')
+    ])  # The logger used to record the training process.
+dist_params = dict(backend='nccl')  # Parameters to setup distributed training, the port can also be set.
+log_level = 'INFO'  # The level of logging.
+
+# load models as a pre-trained model from a given path. This will not resume training.
+# Here, a COCO pretrained model. Could also be saved locally, then no downloading is necessary for each training!
+load_from = 'https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa
+resume_from = None  # Resume checkpoints from a given path, the training will be resumed from the epoch when the checkpoint's is saved.
+# workflow = [('train', 1)]  # Workflow for runner. [('train', 1)] means there is only one workflow and the workflow
+# named 'train' is executed once. The workflow trains the model by 12 epochs according to the total_epochs.
+# work_dir = 'work_dir'  # Directory to save the model checkpoints and logs for the current experiments.
