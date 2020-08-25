9fb5ffd4bd82ff1fb5bb30177e634b9c2fadea8c
meierjonas96@web.de <meierjonas96@web.de>
2020-08-25 10:02:53 +0200

    Fix daatset_type to a python class conform name. Fix data_root to end with a '/'.

--- configs/_base_/datasets/doordetect_detection.py
+++ configs/_base_/datasets/doordetect_detection.py
@@ -1,6 +1,6 @@
-dataset_type = 'DoorDetect-Dataset'
-data_root = 'data/door_detect'
-# seems to be default for every dataset
+dataset_type = 'DoorDetectDataset'  # string has to be conform with python class names!
+data_root = 'data/door_detect/'  # shall end with a '/'
+# img_norm seems to be default for most datasets
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),  # TODO: does this automatically load the images randomly?
