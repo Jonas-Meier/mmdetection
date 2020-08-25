7c2a56bfabce6c0205349d1b649494c94d31c082
meierjonas96@web.de <meierjonas96@web.de>
2020-08-25 14:37:06 +0200

    Add commentary on what is done and why.

--- mmdet/datasets/doordetect.py
+++ mmdet/datasets/doordetect.py
@@ -5,5 +5,8 @@ from .coco import CocoDataset
 
 @DATASETS.register_module()
 class DoorDetectDataset(CocoDataset):
+    # we inherit from CocoDataset since we use COCO-json annotation format. We only need to change the CLASSES value,
+    # since coco.py should already have the necessary tools for training, reading annotations and images, as well as
+    # evaluating the detection task
 
-    CLASSES = ('door', 'handle', 'cabinet door', 'refrigerator door')
+    CLASSES = ('door', 'handle', 'cabinet door', 'refrigerator door')  # we have only these four classes in this dataset
